2d31924103492d58e57437658d46e4496e6675a4
Derron Jackson <derron@Derrons-MacBook-Pro.local>
2023-09-26 19:59:21 -0500

    Created a dealership app

--- src/Dealership.java
+++ src/Dealership.java
@@ -9,29 +9,43 @@ public class Dealership {
         System.out.println(" - Select option 'b' to sell a car");
         String option = scan.nextLine();
 
+        switch (option) {
+            case "a" :
+                System.out.println("you chose option " + option);
+                System.out.println("What is your budget?");
+                double budget = scan.nextDouble();
+                if (budget >= 10000) {
+                    System.out.println("Great! A Nissan Altima is available");
+                    System.out.println("\nDo you have insurance? Write 'yes' or 'no'");
+                    scan.nextLine();
+                    String insurance = scan.nextLine();
+                    System.out.println("\nDo you have a license? Write 'yes' or 'no'");
+                    String license = scan.nextLine();
+                    System.out.println("What is your credit score?");
+                    int creditScore = scan.nextInt();
 
+                    if (insurance.equals("yes") && license.equals("yes") && creditScore > 660) {
+                        // if they have insurace, a license, and credit score greater than 660
+                        System.out.println("Sold! Pleasure doing business with you");
+                    } else {
+                        System.out.println("Sorry, we cannot sell you a car.");
+                    }
+                } else {
+                    System.out.println("We're sorry. You are not eligible");
+                }
+                break;
+            case "b" :
+                System.out.println("you chose option " + option);
+                break;
+            default:
+                System.out.println("invalid option " + option);
+        }
         /******************Part 1******************/
         System.out.println("you chose option " + option);
         System.out.println("you chose option " + option);
         System.out.println("invalid option " + option);
 
         /******************Case A******************/
-
-        System.out.println("What is your budget?");
-
-        // budget greater than or equal to 10000
-        System.out.println("Great! A Nissan Altima is available");
-        System.out.println("\nDo you have insurance? Write 'yes' or 'no'");
-        String insurance = scan.nextLine();
-        System.out.println("\nDo you have a license? Write 'yes' or 'no'");
-        String license = scan.nextLine();
-        System.out.println("What is your credit score?");
-        int creditScore = scan.nextInt();
-        // if they have insurace, a license, and credit score greater than 660
-        System.out.println("Sold! Pleasure doing business with you");
-
-        // they don't meet budget requirements
-        System.out.println("We're sorry. You are not eligible");
 
         /******************Case B******************/
         System.out.println("What is your car valued at?");
